4022db39cf034b55c15a19fbc4f30b5604d41928
Kethan <kchandrakanth@thapovan-inc.com>
2021-07-21 18:21:15 +0530

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,7 @@ export type Promisable<T> = Promise<T> | T;
 export type Handler<T> = (req: Msg<T>) => Promisable<void>;
 
 declare class Emit<Events extends EventsMap = any> {
+    constructor();
     request<K extends keyof Events>(event: K, data: Events[K]): Promise<Events[K]>;
     subscribe<K extends keyof Events = any>(event: K, cb: Handler<Events[K]>): Emit;
 }
