e6a4399fc0aa73f2f0de5ea1608111700093a9c3
Kethan <kchandrakanth@thapovan-inc.com>
2021-07-21 20:03:02 +0530

    add files array in package.json

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,7 @@ export interface Msg<T = any> {
 
 export type Promisable<T> = Promise<T> | T;
 export type Handler<T = any> = (req: Msg<T>) => Promisable<void>;
-export default class Emit<T extends EventsMap = any> {
+export declare class Emit<T extends EventsMap = any> {
     constructor();
     request<K extends keyof T>(event: K, data: T[K]): Promise<T[K]>;
     subscribe<K extends keyof T>(event: K, cb: Handler<T[K]>): Emit;
